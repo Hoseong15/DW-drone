9a60b6d391a70d400e2e1889c791306f20a88a17
Hoseong15 <dlsghtjd3982@gamil.com>
2023-12-04 14:03:06 +0900

    🚩 freetalk

---
+++ 12월/1201javascript/static/javascript/freetalk.js
@@ -0,0 +1,9 @@
+$('#search').on('keyup',function(){
+  var word = $(this).val();
+  
+  $(".detail>tr").filter(
+    function(){
+      $(this).toggle($(this).text().includes(word));
+    }
+  );
+});     
