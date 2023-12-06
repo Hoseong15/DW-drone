30cb48afd7373ee91c93e0fd1960fa80354e0ff9
Hoseong15 <dlsghtjd3982@gamil.com>
2023-12-06 15:58:07 +0900

    📌 function(window)

--- 12월/1206javascript/static/javascript/table.js
+++ 12월/1206javascript/static/javascript/table.js
@@ -1,26 +1,30 @@
-$('#make').on('click', function(){
+$('#size').attr('disabled', true);
+$('#color').attr('disabled', true);
+
+$('#make').on('click', function () {
   openPage('make');
 });
 
 
-$('#size').on('click', function(){
+$('#size').on('click', function () {
+  // if($('#draw>table').length) 
   openPage('size');
 });
 
 
-$('#color').on('click',function(){
+$('#color').on('click', function () {
   openPage('color');
 });
 
 
-function openPage(page){
+function openPage(page) {
 
   var sheight = screen.availHeight;
   var swidth = screen.availWidth;
   // 새 페이지 - window.open(주소, 대상, 옵션)
   // 대상(target) - _blank(새로운), _self(첫 페이지), _parent
-  var child = window.open(page+".html", "",
-  "width=500,height=300,top="+(sheight/2-150)+",left="+(swidth/2-250)+"");
+  var child = window.open(page + ".html", "",
+    "width=500,height=300,top=" + (sheight / 2 - 150) + ",left=" + (swidth / 2 - 250) + "");
 }
 
 // 브라우저에는 Dom이라는 구조를 시용하여 페이지를 표시한다.
