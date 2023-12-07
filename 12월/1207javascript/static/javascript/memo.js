8d79171ef81e54c471ff90c4d481ce7ae6cb3e2c
Hoseong15 <dlsghtjd3982@gamil.com>
2023-12-07 11:48:04 +0900

    📌object(memo)

---
+++ 12월/1207javascript/static/javascript/memo.js
@@ -0,0 +1,15 @@
+// <i class="bi bi-x-lg"></i>
+
+
+// 생성자 함수
+
+function memo(num, content, importance ) {
+  this.num = num;
+  this.content = content;
+  this.importance = importance;
+}
+
+function importStyle(text, color) {
+ this.text = text;
+ this.color = color;
+}
